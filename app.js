f150fa252a6b60d87ceb41921530dc2f8cfd7612
nickpaolino <nick_paolino@yahoo.com>
2021-03-29 18:56:23 -0400

    add button works! still need some tweeking

---
+++ app.js
@@ -0,0 +1,25 @@
+const newItem = document.getElementById('add-btn').addEventListener('click', addListItem);
+const lineThru = document.getElementById('list-item').addEventListener('click', addStrike);
+
+function addListItem(event) {
+    event.preventDefault();
+    const input = getInput()
+    const newLI = document.createElement('LI');
+    newLI.innerHTML = input;
+    newLI.classList.add('list-item');
+    document.getElementById('list').appendChild(newLI);
+ 
+ }
+
+
+function getInput() {
+    return document.getElementById('new-task').value;
+}
+
+// works but need to add strike
+function addStrike(event) {
+    event.preventDefault();
+    console.log('clicked');
+     
+}
+
