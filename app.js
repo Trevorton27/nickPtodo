3811dd3b56e79e253cb8d298912a7e7a091a9601
nickpaolino <nick_paolino@yahoo.com>
2021-04-02 21:21:07 -0400

    almost finished

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-const taskList = document.getElementById('list');
-const newItem = document.getElementById('add-btn').addEventListener('click', addListItem);
+
+document.getElementById('add-btn')
+.addEventListener('click', addListItem);
 
 function addListItem(event) {
-    event.preventDefault();
+    event.preventDefault()
     const input = document.getElementById('new-task');
-    // Preventing Empty List Item // "required" in (HTML) was not functioning "??"
-    if (input.value == ''){
-        alert("Please Enter Some Text!!!")
-    }
-    else {
-    const newLI = document.createElement('LI');
-    newLI.textContent = input.value;   
-    newLI.setAttribute('id', 'list-item');
+    const text = input.value;
+    const newLI = document.createElement('li');
+    const taskList = document.getElementById('list');
+    const deleteButton = createDeleteButton(newLI);
+    const completeButton = createCompleteButton(newLI);
 
-    const deleteButton = document.createElement('BUTTON');
-    deleteButton.textContent = "X"
-    deleteButton.setAttribute('id', 'dlt-btn');
-    deleteButton.classList.add('delete');  
+    newLI.textContent = text;   
 
-    const completeButton = document.createElement('BUTTON');
-    completeButton.textContent = "0"
-    completeButton.setAttribute('id', 'complete-btn');
-  
-    taskList.append(newLI);
     newLI.append(deleteButton);
     newLI.append(completeButton);
-  
+    taskList.append(newLI);
+    
+    newLI.setAttribute('id', 'list-item');
+
     input.value = '';
-    console.log(newLI)
-    }
  }
 
 
-function deleteItem(event) {
-    event.preventDefault()
-    const deleteListItem = document.getElementById('dlt-btn')
-    deleteListItem.addEventListener('click', function () {
-        if (event.target.classList.contains('delete')){
-            let li = event.target.parentElement;
-            taskList.removeChild(li)
-            }
-        })
-    }
+function createDeleteButton(newLI) {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "X";
+    deleteButton.setAttribute('id', 'dlt-btn');
 
-function lineThruText(event) {
-    event.preventDefault();
-}
+    deleteButton.addEventListener('click', () => {
+        newLI.remove()
+    })
+    return deleteButton;
+ }
+  
 
+function createCompleteButton(newLI) {
+    
+    const completeButton = document.createElement('button');
+    completeButton.textContent = "O";
+    completeButton.setAttribute('id', 'complete-btn');
+
+    completeButton.addEventListener('click', (event) => {
+        event.preventDefault()
+        newLI.style.textDecoration = 'line-through';
+    })
+    return completeButton;
+ }
